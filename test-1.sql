cfaf8aa9b344229d00627e3b2e2d1e9340155610
Christian Lindig <christian.lindig@cloud.com>
2025-05-29 16:34:06 +0100

    Use better names in test-1.sql
    Signed-off-by: Christian Lindig <christian.lindig@cloud.com>

--- test-1.sql
+++ test-1.sql
@@ -3,11 +3,11 @@
 .import "test-1.csv" test
 .mode table
 select 
-  cast(x as text) as x, 
-  y, 
-  hms(x) as z, 
-  cast (round(duration(y),3) as text) as d 
+  cast(x as text) as x,
+  y,
+  hms(x) as h,
+  cast (round(duration(y),3) as text) as d
 from test
 where
-  x != d or y != z
+  x != d or y != h
 ;
